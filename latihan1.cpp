76102e3b30bf775f6f0334e8f1bbcbca3e3e8f4e
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-03-06 16:40:39 -0800

    tes

---
+++ latihan1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // begin
+// numeric Luas,p,l
+double Luas ,p ,l;
+// display 'masukkan panjangnya ='
+cout <<"Masukan Panjangnya = ";
+// accept p
+cin >> p;
+// display 'masukkan lebarnya= '
+cout << "Masukkan Lebarnya =";
+// accept 1
+cin >> l;
+// compute Luas =p*l
+Luas = p*l;
+// display "luasnya ="+Luas
+cout << "Luasnya =" << Luas;
+// end
+
+}
